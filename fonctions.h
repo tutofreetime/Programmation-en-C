bb8f097a95ca2f960ef75608e32c2aa524802fa0
duramana <duramana.kalumvuati@laposte.net>
2018-02-02 20:11:09 +0100

    Initialisation

---
+++ fonctions.h
@@ -0,0 +1,76 @@
+
+
+int puissance2(int a, int n)
+{
+    int i, s=1;
+    for(i = 0 ; i < n ; i++)
+        s *=2;
+    s = s * a;
+    
+    return s;
+}
+char dtochar(int n)
+{
+    return (n<10)?n+'0':(n-10) +'A';
+}
+void conversion(int *tab, int taille, int n)//n est la puissance de la base 2^n
+{
+    int i;
+    int tailleAux = taille/n + (taille % n != 0);
+    int *tabaux =(int *)malloc(tailleAux*sizeof(int));
+    if(tabaux == NULL)
+        exit(-1);
+    for(i = 0 ; i < taille; i++)
+        tabaux[i/n] = tab[i]<<(i%n);
+
+    for(i = 0 ; i < tailleAux ; i++)
+        printf(" %c ",dtochar(tabaux[i]));
+}
+float convers(int *tab, int precision)
+{
+    int i,n=0;
+    float f;
+
+    for(i = precision-1; i<=0;i--)
+        n+=tab[31-i]<<i;
+    f= *(float *)&n;
+    return f;
+
+}
+
+void afficheTab(int tab[], int taille)
+{
+    int i;
+    for(i = 0 ; i < taille; i++)
+        printf(" %d ",tab[i]);
+}
+
+int sommetab(int * tab, int taille)
+{
+    int i;
+    int s= 0;
+    for(i= 0; i<taille;i++)
+        s +=tab[i];
+    return s;
+}
+
+float conversFloat(int *tab, int precision)
+{
+    int signe =tab[0];
+    int exposant[8];
+    int mantisse[23];
+    int i;
+    float f =0.0;
+    //Exporter l'exposant
+    int exp = 8/2 + 
+    for(i = 1; i < 9; i++){
+        exposant[i] = tab[i];
+    }
+    //Exporter la mantisse
+    for(; i < precision ; i++)
+        mantisse[i] = tab[i];
+
+    
+    return f;
+
+}
---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "fonctions.h"
+
+int main()
+{
+
+
+int tab2[32]= {0,1,0,0,0,0,1,0,1,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+int taille2 = 32;
+printf("%f\n",convers(tab2,taille2));
+//conversion(tab,taille,a);
+printf("\n");
+
+    return 0;
+}
