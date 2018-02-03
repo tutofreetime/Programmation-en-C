70d58f53b88525d0a7de76f5ce74c276c6cc658f
maison2 <filotyty@gmail.com>
2018-02-03 15:55:27 +0100

    Voir la difference en GIT

--- main.c
+++ main.c
@@ -5,11 +5,12 @@
 int main()
 {
     int tab1[32] = {0,1,0,0,0,0,1,0,1,1,0,1,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    //int tab2[32] = {0,1,0,0,0,0,0,1,1,0,0,0,1,1,0,1,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0};
     int quatre[32] = {0,1,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
     int taille2 = 32;
-    printf("%f \n",methodeSimple(tab1,taille2));
-    printf("%f \n",methodeSimple(quatre,taille2));
+    printf("%f \n", conversFloat(tab1, taille2));
+    printf("%f \n", methodeSimple(quatre, taille2));
+    printf("%f \n", methodeSimple(tab1, taille2));
+    printf("%f \n", conversFloat(quatre, taille2));
 
     return 0;
 }
